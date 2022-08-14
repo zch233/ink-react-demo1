c02f7d772370d655a355b75f41c44df5139cb180
zhangchao <529743595@qq.com>
2022-08-14 22:50:25 +0800

    feat: update style & add formData

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Box, render} from 'ink';
+import {Box, Newline, render, Text} from 'ink';
 import SelectInput from 'ink-select-input';
 
 interface CodeUpItem {
@@ -11,26 +11,48 @@ interface CodeUp extends CodeUpItem {
   branch: CodeUpItem[];
 }
 
-const Counter = () => {
-  const codeUp: CodeUp[] = [
-    {
-      label: 'vue3-vite',
-      value: 'git',
-      branch: [{label: 'ts', value: 'master'}, {label: 'js', value: 'master-js'}, {label: 'ant-design-js', value: 'master-js-admin'}, {label: 'element-plus-ts', value: 'element-plus-ts'}]
-    },
-    {
-      label: 'vue3-cli',
-      value: 'git:',
-      branch: [{label: 'js', value: 'master'}, {label: 'ant-design-js', value: 'pc'}, {label: 'vant-js', value: 'mobile'}, {label: 'element-plus-js', value: 'main/adminTemplate'}]
-    }
-  ]
+const codeUp: CodeUp[] = [
+  {
+    label: 'vue3-vite',
+    value: 'git',
+    branch: [
+      {label: '基础模版(ts)', value: 'master'},
+      {label: '基础模版(js)', value: 'master-js'},
+      {label: 'ant-design-js', value: 'master-js-admin'},
+      {label: 'element-plus-ts', value: 'element-plus-ts'},
+    ]
+  },
+  {
+    label: 'vue3-cli',
+    value: 'git:',
+    branch: [
+      {label: 'js', value: 'master'},
+      {label: 'ant-design-js', value: 'pc'},
+      {label: 'vant-js', value: 'mobile'},
+      {label: 'element-plus-js', value: 'main/adminTemplate'},
+    ]
+  }
+]
 
+const Counter = () => {
+  const [formData, setFormData]  = useState<{label: string; value:Partial<CodeUpItem>}[]>([
+    {label: '请选择框架', value: {}},
+    {label: '请选择模版', value: {}},
+    {label: '请输入名称', value: {}},
+  ])
+  const [step, setStep] = useState(0)
   const [selector, setSelector] = useState<CodeUpItem[]>(codeUp.map(({branch, ...v}) => v))
   const handleSelect = (item: CodeUpItem) => {
-    setSelector(codeUp.find(v => v.value === item.value)!.branch)
+    const branch = codeUp.find(v => v.value === item.value)?.branch
+    setStep(step + 1)
+    setFormData(formData.slice(0, step).concat([{...formData[step], value: item}]).concat(formData.slice(step + 1)))
+    setSelector(branch || [])
   }
-  return <Box>
-    <SelectInput items={selector} onSelect={handleSelect} />
+  return <Box flexDirection="column">
+    <Text>
+      {formData.slice(0, step + 1).map((v, i) => <Text key={v.label} color={'green'}><Text>{formData[i].label}:{formData[i].value.label}</Text>{formData[i].value.label && <Newline />}</Text>)}
+    </Text>
+    {selector.length !== 0 && <SelectInput items={selector} onSelect={handleSelect} />}
   </Box>
 };
 
